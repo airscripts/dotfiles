744aeecdaf475c26e50ab84d1012f843ece350a9
Airscript <francesco@airscript.it>
2022-01-27 09:18:11 +0100

    feat(nvim): add indent-blankline plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -15,4 +15,6 @@ return require('packer').startup(function()
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
+
+  use "lukas-reineke/indent-blankline.nvim"
 end)
