c96d24b52a099313b13183a713d3326b4087742a
Airscript <francesco@airscript.it>
2022-01-30 02:56:40 +0100

    fix(nvim): missing parameter to cp command in nvim.sh

--- nvim.sh
+++ nvim.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cp .config/nvim ~/.config/
+cp -r .config/nvim ~/.config/
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 nvim
