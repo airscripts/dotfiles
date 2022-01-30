e59e7daadc648670c4881a7b9c19e2f22ff91e65
Airscript <francesco@airscript.it>
2022-01-30 02:39:11 +0100

    fix(vim): add missing cp .vimrc inside vim.sh

--- vim.sh
+++ vim.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vimrc ~/
+git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c "PluginInstall"
