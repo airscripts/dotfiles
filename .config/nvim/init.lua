6616a01ac434385aa2cc504a8e3301fd899a5acc
Airscript <francesco@airscript.it>
2022-09-28 00:37:43 +0200

    refactor(nvim): start porting to vim api init file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,81 +1,73 @@
 vim.g.mapleader = "<Space>"
+vim.g.oscyank_term = "default"
 
 vim.o.tabstop = 2
-vim.o.shiftwidth = vim.o.tabstop
+vim.o.mouse = "a"
+vim.o.number = true
+vim.o.hidden = true
+vim.o.showcmd = true
+vim.o.smartcase = true
+vim.o.incsearch = true
+vim.o.autowrite = true
 vim.o.modifiable = true
+vim.o.ignorecase = true
+vim.o.background = "dark"
+vim.o.termguicolors = true
+vim.o.shiftwidth = vim.o.tabstop
+vim.o.completeopt = "menu,menuone,noselect"
+
+vim.cmd("filetype on")
+vim.cmd("colorscheme catppuccin")
+vim.cmd("filetype plugin indent on")
+vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
 
 vim.cmd([[
-set number
-set hidden
-set mouse=a
-set showcmd
-set showmatch
-set smartcase
-set incsearch
-set autowrite
-set ignorecase
-set nocompatible
-set termguicolors
-set background=dark
-set rtp+=~/.vim/bundle/Vundle.vim
-set completeopt=menu,menuone,noselect
+	if has("syntax")
+		syntax on
+	endif
+]])
 
-filetype on               
-runtime! debian.vim
-colorscheme catppuccin
-filetype plugin indent on
-let g:oscyank_term = 'default'
-hi Normal guibg=NONE ctermbg=NONE
+vim.cmd([[
+	map <silent> <C-s> :w<CR>
+	map <silent> <C-c> :OSCYank<CR>
+	map <silent> <C-p> :Telescope<CR>
+	map <silent> <C-f> :Telescope find_files<CR>
+	map <silent> <C-t> :NvimTreeToggle<CR><C-w>w
+	inoremap <S-Tab> <C-d>
+	inoremap <silent><C-j> <Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>
+	inoremap <silent>f<C-j> :ToggleTerm direction=float<CR>
+	vnoremap <Tab> >>
+	vnoremap <S-Tab> <<
+	tnoremap <esc> <C-\><C-N>
+	nnoremap <silent>[bn :bn<CR>
+	nnoremap <silent>[bp :bp<CR>
+	nnoremap <silent>[nb :tabnew<CR>
+	nnoremap <silent>[bd :bd<CR>:bp<CR>
+	nnoremap <silent><C-j> <Cmd>exe v:count1 . "ToggleTerm"<CR>
+	nnoremap <silent>f<C-j> :ToggleTerm direction=float<CR>
+]])
 
-if has("syntax")
-  syntax on
-endif
-
-map <silent> <C-s> :w<CR>
-map <silent> <C-c> :OSCYank<CR>
-map <silent> <C-p> :Telescope<CR>
-map <silent> <C-f> :Telescope find_files<CR>
-map <silent> <C-t> :NvimTreeToggle<CR><C-w>w
-
-inoremap <S-Tab> <C-d>
-inoremap <silent><C-j> <Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>
-inoremap <silent>f<C-j> :ToggleTerm direction=float<CR>
-
-vnoremap <Tab> >>
-vnoremap <S-Tab> <<
-
-tnoremap <esc> <C-\><C-N>
-
-nnoremap <silent>[bn :bn<CR>
-nnoremap <silent>[bp :bp<CR>
-nnoremap <silent>[nb :tabnew<CR>
-nnoremap <silent>[bd :bd<CR>:bp<CR>
-nnoremap <silent><C-j> <Cmd>exe v:count1 . "ToggleTerm"<CR>
-nnoremap <silent>f<C-j> :ToggleTerm direction=float<CR>
-
-if filereadable("/etc/vim/vimrc.local")
-  source /etc/vim/vimrc.local
-endif
-
-call vundle#begin()
-Plugin 'VundleVim/Vundle.vim'
-Plugin 'tpope/vim-fugitive'
-call vundle#end()
+vim.cmd([[
+	set rtp+=~/.vim/bundle/Vundle.vim
+	call vundle#begin()
+	Plugin 'VundleVim/Vundle.vim'
+	Plugin 'tpope/vim-fugitive'
+	call vundle#end()
 ]])
 
 require("plugins")
 require("core.mason")
 require("core.mason-lspconfig")
-require("core.bufferline")
-require("core.cmp")
 require("core.lspconfig")
+require("core.cmp")
+require("core.treesitter")
 require("core.dap")
 require("core.dap-ui")
-require("core.alpha")
-require("core.lualine")
 require("core.telescope")
 require("core.nvim-tree")
-require("core.treesitter")
+require("core.alpha")
+require("core.bufferline")
+require("core.lualine")
 require("core.toggleterm")
 require("core.catppuccin")
 require("core.which-key")
