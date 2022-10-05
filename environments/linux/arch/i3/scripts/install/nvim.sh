3716ebb60ed0d26d38a4702ab41d8d4c3a3acb86
Airscript <francesco@airscript.it>
2022-10-05 21:36:47 +0200

    refactor(scripts): remove package install from nvim install script

--- environments/linux/arch/i3/scripts/install/nvim.sh
+++ environments/linux/arch/i3/scripts/install/nvim.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-sudo pacman -S neovim
 cp -r ../core/nvim ~/.config/
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-nvim -c PackerSync
